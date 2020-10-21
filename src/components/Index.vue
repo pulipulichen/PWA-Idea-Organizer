f4ca61bda096d383c3a73a40be1eca36f4bad87d
Pulipuli Chen <blog@pulipuli.info>
2020-10-21 20:32:07 +0800

    1021 close #72 完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,10 @@
 <template src="./Index.html"></template>
 <script src="./Index.js"></script>
 <style src="./IndexLoading.less" lang="less" scoped></style>
+
 <style src="./IndexEditorContent.less" lang="less" scoped></style>
+<style src="./IndexEditorContent.mobile.less" lang="less" scoped></style>
+
 <style src="./IndexEditorMargin.less" lang="less" scoped></style>
 <style src="./IndexEditorGlobal.less" lang="less"></style>
 <i18n src="./Index.yaml" lang="yaml"></i18n>

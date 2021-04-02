f5dc9d4dd7f0f4ccd7e5b11c21957c413b58b365
Pulipuli Chen <blog@pulipuli.info>
2021-04-02 21:57:09 +0800

    0402 fix bugs - Make music playlist higher

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const RUNTIME = 'runtime';
-const PRECACHE = 'precache-v2021-0322-092839'
+const PRECACHE = 'precache-v2021-0402-0000000'
 
 /**
  * How to build cache list?
